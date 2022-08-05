643f482f3bdc57a21cc0cef593a0f77b47300a8e
Jannik Bamberger <mail@jbamberger.de>
2022-08-06 01:05:30 +0200

    Fix test cases

--- staticmap/tests.py
+++ staticmap/tests.py
@@ -1,19 +1,18 @@
 from unittest import TestCase
-
-from staticmap import StaticMap
+from staticmap import staticmap
 
 
 class LonLatConversionTest(TestCase):
     def testLon(self):
         for lon in range(-180, 180, 20):
             for zoom in range(0, 10):
-                x = _lon_to_x(zoom)
-                l = _x_to_lon(zoom)
+                x = staticmap._lon_to_x(lon, zoom)
+                l = staticmap._x_to_lon(x, zoom)
                 self.assertAlmostEqual(lon, l, places=5)
 
     def testLat(self):
         for lat in range(-89, 89, 2):
             for zoom in range(0, 10):
-                y = _lat_to_y(zoom)
-                l = _y_to_lat(zoom)
+                y = staticmap._lat_to_y(lat, zoom)
+                l = staticmap._y_to_lat(y, zoom)
                 self.assertAlmostEqual(lat, l, places=5)
